17aead8d2ca8205196b8bae576ecb9a314601a9b
Cro-Magnon <robertoruizicochea@gmail.com>
2022-09-21 01:40:02 -0500

    Update ui.R

--- ui.R
+++ ui.R
@@ -25,11 +25,28 @@ shinyUI(fluidPage(
                
                sidebarPanel(
                  
-                 textInput("ticker", "Ticker", value = 'ROK'),
+                 textInput("ticker", "Ticker", value = ''),
                  
                  awesomeRadio(
                    inputId = "upside",
                    label = "UPSIDE",
+                   choices = c("P/E" = 'P/E' ,
+                               "P/E NTM" = "P/E NTM",
+                               "P/E NTM vs S&P500" = "P/E NTM vs S&P500",
+                               "P/B" = "P/B",
+                               "P/CF"= "P/CF",
+                               "EV/Sales" = "EV/Sales" ,
+                               "EV/EBITDA" = "EV/EBITDA",
+                               "EV/EBITDA vs S&P500" = "EV/EBITDA vs S&P500"#,
+                               #"Div Yield" = "Div Yield"
+                   ),
+                   
+                   selected = "P/E",
+                   status = "warning"
+                 ),
+                 awesomeRadio(
+                   inputId = "downside",
+                   label = "DOWNSIDE",
                    choices = c("P/E" = 'P/E' ,
                                "P/E NTM" = "P/E NTM",
                                "P/E NTM vs S&P500" = "P/E NTM vs S&P500",
@@ -52,15 +69,18 @@ shinyUI(fluidPage(
                            "End Date",
                            Sys.Date(),
                            format = "yyyy-mm-dd"
-                 )
+                 ),
+                 actionButton("Calculate", "Calculate")
+                 
                ),
                
                # Show a plot of the generated distribution
                mainPanel(
                 tabsetPanel(
                   tabPanel(
-                    title = "Plot",
-                    plotOutput("plot_up")
+                    title = "Plots",
+                    plotOutput("plot_up"),
+                    plotOutput("plot_down")
                   ),
                   tabPanel(
                     title = "Backtest",
@@ -69,58 +89,7 @@ shinyUI(fluidPage(
                   )
                 )
                )
-             ),
-             
-             
-             sidebarPanel(
-               
-               awesomeRadio(
-                 inputId = "downside",
-                 label = "DOWNSIDE",
-                 choices = c("P/E" = 'P/E' ,
-                             "P/E NTM" = "P/E NTM",
-                             "P/E NTM vs S&P500" = "P/E NTM vs S&P500",
-                             "P/B" = "P/B",
-                             "P/CF"= "P/CF",
-                             "EV/Sales" = "EV/Sales" ,
-                             "EV/EBITDA" = "EV/EBITDA",
-                             "EV/EBITDA vs S&P500" = "EV/EBITDA vs S&P500"#,
-                             #"Div Yield" = "Div Yield"
-                 ),
-                 selected = "P/E",
-                 status = "warning"
-               ),
-               dateInput("start_down",
-                         "Start Date",
-                         Sys.Date() - years(10),
-                         format = "yyyy-mm-dd"
-               ),
-               dateInput("end_down",
-                         "End Date",
-                         Sys.Date(),
-                         format = "yyyy-mm-dd"
-               )
-             ),
-             
-             # ToDo: there need to be two plots here....one for upside, one for downside with
-             # related data presentations....
-             # Show a plot of the generated distribution
-             #
-             
-             mainPanel(
-              tabsetPanel(  
-                tabPanel(
-                  title = 'Plot',
-                  plotOutput("plot_down")
-                ),
-                tabPanel(
-                  title = 'Backtest',
-                  dataTableOutput('table_downside'),
-                  dataTableOutput('backtest_downside')
-                )
-              )
              )
-             
     ),
     
     tabPanel("Index")
